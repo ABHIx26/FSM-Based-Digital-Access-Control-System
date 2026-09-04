6180759b350f3aec1e2bca8273c32e46f4d727f1
Abhinav Anand <abhinav852101@gmail.com>
2026-09-04 23:49:18 +0530

    Add simulation script for Verilog design

--- sim/run_simulation.sh
+++ sim/run_simulation.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# Compile Verilog design and testbench
-iverilog -o access_control_sim ../src/access_control.v ../tb/access_control_tb.v
+# Compile the Verilog design and testbench
+iverilog -o access_control_sim \
+    ../src/access_control.v \
+    ../tb/access_control_tb.v
 
-# Run simulation
+# Run the simulation
 vvp access_control_sim
 
-# Open waveform
+# Open the generated waveform
 gtkwave access_control.vcd
